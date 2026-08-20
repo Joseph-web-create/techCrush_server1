c2fa471b0420e497f6dbc39c47f39a64cd014272
tj <aacejovi@gmail.com>
2026-08-20 20:29:08 +0100

    first commit

--- index.js
+++ index.js
@@ -5,24 +5,22 @@ const port = 5025;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Server is running",
-  });
-});
-
+// Home route
 app.get("/home", (req, res) => {
   res.status(200).json({ message: "Welcome to the home page" });
 });
 
+// Contact us route
 app.get("/contact-us", (req, res) => {
   res.status(200).json({ message: "Contact us" });
 });
 
+// About route
 app.get("/about", (req, res) => {
   res.status(200).json({ message: "about us" });
 });
 
+// Start Server
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
